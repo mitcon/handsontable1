6d7ba8f997a211d07f0449b958fe652f34dd45b2
mitcon <koizumi741@gmail.com>
2019-09-25 21:20:13 +0900

    help

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <div id="app">
-    <hot-table :settings="hotSettings"></hot-table>
-  </div>
+  <hot-table :settings="hotSettings">
+    <hot-column :width="250">
+      <custom-renderer hot-renderer></custom-renderer>
+    </hot-column>
+  </hot-table>
 </template>
 
 <script>
@@ -9,21 +11,38 @@ import Vue from "vue";
 import { HotTable } from "@handsontable/vue";
 import Handsontable from "handsontable";
 
+const CustomRenderer = {
+  template:
+    '<div><i style="color: #a9a9a9">Row: {{row}}, column: {{col}},</i> value: {{value}}</div>',
+  data: function() {
+    return {
+      hotInstance: null,
+      TD: null,
+      row: null,
+      col: null,
+      prop: null,
+      value: null,
+      cellProperties: null
+    };
+  }
+};
+
 export default {
-  el: "#app",
+  el: "#custom-renderer-example",
   data: function() {
     return {
       hotSettings: {
-        data: Handsontable.helper.createSpreadsheetData(6, 10),
-        colHeaders: true,
-        licenseKey: "non-commercial-and-evaluation"
+        data: Handsontable.helper.createSpreadsheetData(10, 10),
+        licenseKey: "non-commercial-and-evaluation",
+        autoRowSize: false,
+        autoColumnSize: false
       }
     };
   },
   components: {
-    HotTable
+    HotTable,
+    HotColumn,
+    CustomRenderer
   }
 };
 </script>
-
-<style src="../node_modules/handsontable/dist/handsontable.full.css" />
